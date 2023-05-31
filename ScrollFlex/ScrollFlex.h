0e08ed901c024d311e22507dd3c3cfbeee92dee5
Marta Korol <korolmarta490@gmail.com>
2023-05-31 18:13:24 +0300

    Initial Commit

---
+++ ScrollFlex/ScrollFlex.h
@@ -0,0 +1,18 @@
+//
+//  ScrollFlex.h
+//  ScrollFlex
+//
+//  Created by Marta Korol on 31.05.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ScrollFlex.
+FOUNDATION_EXPORT double ScrollFlexVersionNumber;
+
+//! Project version string for ScrollFlex.
+FOUNDATION_EXPORT const unsigned char ScrollFlexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScrollFlex/PublicHeader.h>
+
+
